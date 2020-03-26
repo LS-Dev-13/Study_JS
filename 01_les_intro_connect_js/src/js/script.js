4082d4f09d33254eff58b22396c81927b25e36f6
Leonid Solovyov <leonid.solovyov13@gmail.com>
2020-03-26 23:06:34 +0200

    master saving after del branch

---
+++ 01_les_intro_connect_js/src/js/script.js
@@ -0,0 +1,16 @@
+  let  // Объявляем переменную (Var (es5), Let, Const (es6))
+  money = 1,
+  income = 2,
+  addExpenses = 3,
+  deposit = 4,
+  mission = 5,
+  period = 6; 
+  
+alert('Test Message!'); //Выводим сообщение в модальное окно
+console.log(money, income, addExpenses, deposit, mission, period); /* 
+выводим переменные в консоль */
+
+console.log(period); //test 4 commit
+
+
+
