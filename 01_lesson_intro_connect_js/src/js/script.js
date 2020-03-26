1d94778efd8e487f4a442c15228b8ee43ebec101
Leonid Solovyov <leonid.solovyov13@gmail.com>
2020-03-26 22:29:17 +0200

    lesson01

--- 01_lesson_intro_connect_js/src/js/script.js
+++ 01_lesson_intro_connect_js/src/js/script.js
@@ -1,4 +1,4 @@
-  let  // Объявляем переменную (Var (es5), Let, Const (es6))
+gitlet  // Объявляем переменную (Var (es5), Let, Const (es6))
   money = 1,
   income = 2,
   addExpenses = 3,
@@ -9,8 +9,6 @@
 alert('Test Message!'); //Выводим сообщение в модальное окно
 console.log(money, income, addExpenses, deposit, mission, period); /* 
 выводим переменные в консоль */
-
-console.log(period); //test 4 commit
 
 
 
