d39bc63c61dcc8bd1e3625befc536aa0fade4823
Leonid Solovyov <leonid.solovyov13@gmail.com>
2020-04-09 20:24:17 +0300

    push les09

--- 09_DOM/required/src/js/index.js
+++ 09_DOM/required/src/js/index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// API - application programming interface. в основном реализованы в виде объектов. 
-// DOM - document object model. Для работы с элементыами на странице.
-// BOM - browser object mode. Для работы браузером.
-// XMLHttpRequest(XHR) объекты для взаимодействия с серверами.
-// структура - корень(document) - узел (html и все внутри) - листья (конечные без детей)
-
-// console.log(document.parentElement);
-// console.log(document.children); // получаем псевдомассив HTML collection
-// Способы получения элементов
 // getElemenetById
 const startСalc = document.getElementById('start');
 // getElementsByTagName
@@ -37,88 +28,8 @@ const salaryAmount = document.querySelector('.salary-amount'),
       targetAmount = document.querySelector('.target-amount'),
       periodSelect = document.querySelector('.period-select');
 
-// console.log(startСalc, buttonAddFirst, buttonAddSecond, additionalIncomeItem, budgetMonthValue, budgetDayValue, expensesMonthValue, additionalIncomeValue, additionalExpensesValue, incomePeriodValue, targetMonthValue, salaryAmount, incomeTitle, incomeAmount, expensesTitle, expensesAmount, expensesItem, depositCheck, depositAmount, depositPercent, targetAmount, periodSelect);
-
-
-// // querySelector по атрибуту
-// console.log(document.querySelector('[title="Основной заголовок"]'));
-
-// // Способы управления элементами
-// const mainHead = document.querySelector('h1');
-// // console.dir(mainHead.getAttribute('title')); // получаем атрибут переменной
-// // работа с атрибутами через setAttribute
-// mainHead.setAttribute('title', 'Test Title');
-// mainHead.setAttribute('style', 'color:blue');
-// // Так как в переменную попадает объект - можно использовать его методы
-// mainHead.title = 'Новый title';
-// console.log(mainHead.title);
-// mainHead.className = 'test_new'; // при таком методе классы заменяются 
-// console.log(mainHead.className);
-// console.log(mainHead);
-// console.log(mainHead.classList); // получаем DOMTokenList и можем посмотреть досутпные методы
-// // Самый популярные методы classList = contains, add, remove, toggle
-// mainHead.classList.add('class_add');
-// console.log(mainHead.classList);
-// mainHead.classList.remove('test_new');
-// console.log(mainHead.classList);
-// //contains делает проверку есть ли класс у данного элемента - возвращает булевое значение
-// console.log(mainHead.classList.contains('test_new'));
-// console.log(mainHead.classList.contains('class_add'));
-// // toggle проверяет есть ли элемент и соответвенно удаляет или добавляет его.
-// mainHead.classList.toggle('class_add');
-// console.log(mainHead.classList.contains('class_add'));
-// mainHead.classList.toggle('class_add');
-// console.log(mainHead.classList.contains('class_add'));
-// mainHead.classList.toggle('class_add');
-// console.log(mainHead.classList.contains('class_add'));
-// mainHead.classList.toggle('class_add');
-// console.log(mainHead.classList);
-
-// // style мы работает со свойствами только с DOM (без файла css, елементов style в HTML и т.д.)
-// console.log(mainHead.style);
-// mainHead.style.color = '#fff';
-// document.body.style.backgroundColor = '#000';
-// mainHead.style.fontSize = '40px';
-// // получение свойcтв файла css
-// // getComputedStyle (api) метод возвращает объект сразу со всеми свойствами
-// const computedStyleMainHead = getComputedStyle(mainHead);
-// // после запятой в параметрах можно получать псевдоэлементы
-// // const computedStyleMainHeadAfter = getComputedStyle(mainHead, 'after');
-// console.log(computedStyleMainHead.marginTop);
-
-// // queryselector можно юзать и на самих єлементах
-// // вариант 1. получить отдельный эллемент и после внутри него найти нужные.
-// const label2 = document.querySelector('label');
-// let deposit = label2.querySelectorAll('.btn_plus');
-// console.log('deposit: ',  deposit);
-// // вариант 2 используем вложенность селекторов
-// deposit = document.querySelectorAll('label>.btn_plus'); // знак > = дочерниеэлементы. а пробел - просто внутри.
-// console.log(deposit);
-
-
-
-
-
-
-
-
-
-
-
-
-// выведем всконсоль для каждого элемента сколько справа от него элементов меньше.
-// const func = function(arr) {
-//   let arr1 = [],
-//       num;
-//     for (let el = 0; el < arr.length; el++) {
-//       num = 0;
-//     for (let i = el; i < arr.length; i++) {
-//       if (arr[i] < arr[el]){
-//         num++;
-//       }
-//     }
-//     arr1.push(num);
-//   } 
-//   return arr1;
-// };
-// console.log(func([1,4,0,3,2,1]));
+/* console.log(startСalc, buttonAddFirst, buttonAddSecond, additionalIncomeItem, 
+budgetMonthValue, budgetDayValue, expensesMonthValue, additionalIncomeValue, 
+additionalExpensesValue, incomePeriodValue, targetMonthValue, salaryAmount, 
+incomeTitle, incomeAmount, expensesTitle, expensesAmount, expensesItem, 
+depositCheck, depositAmount, depositPercent, targetAmount, periodSelect); */
